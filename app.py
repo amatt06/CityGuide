b301017e329b52910cdd00d036be528562a248aa
amatt06 <S3853376@student.rmit.edu.au>
2023-11-10 14:52:33 +1000

    view base template

--- app.py
+++ app.py
@@ -1,11 +1,31 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def login():  # put application's code here
+    return render_template('login.html')
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+@app.route('/logout')
+def logout():
+    return render_template('login.html')
+
+
+@app.route('/preferences')
+def preferences():
+    return render_template('preferences-input.html')
+
+
+@app.route('/results')
+def results():
+    return render_template('results.html')
 
 
 if __name__ == '__main__':
