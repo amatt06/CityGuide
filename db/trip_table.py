e7700c6ea09bc9cfe5c43fd31bc0c16a36a50f66
amatt06 <S3853376@student.rmit.edu.au>
2023-11-21 12:17:54 +1000

    remove botocore

--- db/trip_table.py
+++ db/trip_table.py
@@ -1,6 +1,4 @@
 import boto3
-from boto3.dynamodb.conditions import Key
-from botocore.exceptions import ClientError
 import json
 
 s3 = boto3.client('s3')
@@ -10,66 +8,50 @@ trip_data_bucket = 'city-guide-trip-data'
 
 
 def save_trip(user_email, trip_id, trip_name, trip_notes, s3_url):
-    try:
-        response = trips_table.put_item(
-            Item={
-                'Email': user_email,
-                'TripID': trip_id,
-                'TripName': trip_name,
-                'TripNotes': trip_notes,
-                'S3URL': s3_url,
-            }
-        )
-        print("Save trip successful:", response)
-        return True
-    except ClientError as e:
-        print("Error saving trip:", e)
-        return False
+    response = trips_table.put_item(
+        Item={
+            'Email': user_email,
+            'TripID': trip_id,
+            'TripName': trip_name,
+            'TripNotes': trip_notes,
+            'S3URL': s3_url,
+        }
+    )
+    print("Save trip successful:", response)
+    return True
 
 
 def get_user_trips(user_email):
-    try:
-        response = trips_table.query(
-            KeyConditionExpression='Email = :email',
-            ExpressionAttributeValues={':email': user_email}
-        )
-        return response.get('Items', [])
-    except ClientError as e:
-        print(f"Error getting user trips: {e}")
-        return []
+    response = trips_table.query(
+        KeyConditionExpression='Email = :email',
+        ExpressionAttributeValues={':email': user_email}
+    )
+    return response.get('Items', [])
 
 
 def get_trip_details(email, trip_id):
-    try:
-        response = trips_table.get_item(
-            Key={
-                'Email': email,
-                'TripID': trip_id,
-            }
-        )
-        item = response.get('Item')
-        if item:
-            s3_url = item.get('S3URL')
-            trip_data = get_trip_data_from_s3(s3_url)
-            return trip_data
-    except ClientError as e:
-        print("Error getting trip details:", e)
-    return None
+    response = trips_table.get_item(
+        Key={
+            'Email': email,
+            'TripID': trip_id,
+        }
+    )
+    item = response.get('Item')
+    if item:
+        s3_url = item.get('S3URL')
+        trip_data = get_trip_data_from_s3(s3_url)
+        return trip_data
 
 
 def get_trip_data_from_s3(s3_url):
-    try:
-        bucket, key = get_bucket(s3_url)
+    bucket, key = get_bucket(s3_url)
 
-        response = s3.get_object(Bucket=bucket, Key=key)
-        content = response['Body'].read().decode('utf-8')
+    response = s3.get_object(Bucket=bucket, Key=key)
+    content = response['Body'].read().decode('utf-8')
 
-        trip_data = json.loads(content)
+    trip_data = json.loads(content)
 
-        return trip_data
-    except ClientError as e:
-        print("Error getting trip data from S3:", e)
-    return None
+    return trip_data
 
 
 def get_bucket(s3_url):
--- db/user_table.py
+++ db/user_table.py
@@ -1,38 +1,26 @@
 import bcrypt
 import boto3
-from botocore.exceptions import ClientError
 
 dynamodb = boto3.resource('dynamodb', region_name='ap-southeast-2')
 table = dynamodb.Table('CityGuideUsers')
 
 
 def email_exists(email):
-    try:
-        response = table.get_item(
-            Key={
-                'Email': email
-            }
-        )
-    except ClientError as e:
-        print(f"Error checking email : {e}")
-        return False
+    response = table.get_item(
+        Key={
+            'Email': email
+        }
+    )
 
     return 'Item' in response
 
 
 def match_password(email, password):
-    try:
-        response = table.get_item(
-            Key={
-                'Email': email
-            }
-        )
-    except ClientError as e:
-        print(f"Error checking password: {e}")
-        return False
-
-    if 'Item' not in response:
-        return False
+    response = table.get_item(
+        Key={
+            'Email': email
+        }
+    )
 
     hashed_password = response['Item'].get('Password')
 
